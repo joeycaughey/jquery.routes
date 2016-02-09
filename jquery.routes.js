7b4757aae600daedffceffa7af3eafbca3bcdfab
Joey Caughey <joey.caughey@gmail.com>
2016-02-09 13:05:30 -0500

    Update jquery.routes.js

--- jquery.routes.js
+++ jquery.routes.js
@@ -1,106 +1,105 @@
-
 var Routes = {
-	hash: false,
-	routes: false,
-	parameters: [],
-	reload: function() {
-		var self = this;
-		self.init(self.routes);
-	},
-	init: function(routes) {
-		var self = this;
-		var match = false;
+    hash: false,
+    routes: false,
+    parameters: [],
+    reload: function() {
+        var self = this;
+        self.hash = location.hash.replace("#!/", "");
 
-		self.routes = routes;
-		self.hash = location.hash.replace("#!/", "");
+        self.hash = self.hash.split("?");
+        self.hash = self.hash[0];
+        
+        self.parameters = [];
+        self.match_route(self.routes);
+    },
+    match_route: function(routes) {
+        var self = this;
 
-		$.each(routes, function(uri, route) {
-			if (uri.slice(-1) !== "/") { uri=uri+"/"; }
-			var regex = new RegExp("^"+self.to_regex(uri, route)+"$", 'gi');
-			if (self.hash.match(regex)) {		
-				self.parse(route);
-				self.get_parameters(uri, route);
-				match = true;
-			}
-		});
+        var found = false;
+        $.each(routes, function(uri, route) {
+            var regex = new RegExp("^" + self.to_regex(uri, route) + "$", 'gi');
 
-		if (self.hash == "" || self.hash == "/" || self.hash  == "home/") {
-			self.parse({
-				template: "site/home.html"
-			})
-		} else if (!match) {
-			self.parse({
-				template: self.hash
-			})
-		}
-	},
-	to_regex: function(uri, route) {
-		var output = uri;
-		if (typeof route.parameters !== "undefined") {
-			$.each(route.parameters, function(key, value) {
-				output = output.replace(":"+key, '('+value+')');
-			})
-		}
-		return output;
-	},
-	parse: function(route) {
-		var self = this;
+            if (self.hash.match(regex)) {
+                self.parse(route);
+                self.get_parameters(uri, route);
+                //console.log("Found : ", uri, route);
+                found = true;
+                return true;
+            }
+        });
 
-	
-		if (typeof PreloaderShow === "function") {
-                        PreloaderShow();
-                } 
-                
-		var jqxhr = $.get(route.template, function(html) {
-			$("article > section").html(html);
+        return true;
+    },
+    init: function(routes) {
+        var self = this;
+        self.routes = routes;
+    },
+    to_regex: function(uri, route) {
+        var output = uri;
+        if (typeof route.parameters !== "undefined") {
+            $.each(route.parameters, function(key, value) {
+                output = output.replace(":" + key, '(' + value + ')');
+            })
+        }
+        return output;
+    },
+    parse: function(route) {
+        var self = this;
 
-			if (typeof route.meta !== "undefined") {
-				self.load_meta_information(route.meta);
-			}
-			
-			if (typeof PreloaderHide === "function") {
-	                        PreloaderHide();
-	                } 
-		});
+        route.target = (route.target) ? route.target : "article";
 
+        var jqxhr = $.get(route.template, function(html) {
+            $(route.target).html(html);
 
-		jqxhr.error(function() {
-			$.get("site/errors/404.html", function(html) {
-				$("article > section").html(html)
-					.append($('<div style="clear: both;">'));
-			});
-		});
+            if (typeof route.meta !== "undefined") {
+                self.load_meta_information(route.meta);
+            }
 
-	},
-	get_parameters: function(uri, route) {
-		var self = this;
-		if (typeof route.parameters !== "undefined") {
-			$.each(route.parameters, function(key, regex) {
-				self.parameters[key] = self.hash.match("^"+self.to_regex(uri, route)+"$")[1];
-			});
-		}
-	},
-	load_meta_information: function(meta) {
+            $("html, body").animate({
+                scrollTop: 0
+            }, "slow");
+            reload_ui();
+            $("article > section#content-first").css("visibility", "visible");
 
-		if (typeof meta.title !== "undefined") {
-			$("title").html(meta.title);
-			$("meta[name=title]").attr("content", meta.title);
-		}
+        });
 
-		if (typeof meta.keywords !== "undefined") {
-			$("meta[name=keywords]").attr("content", meta.keywords);
-		}
+        jqxhr.error(function() {
+            $.get("site/errors/404.html", function(html) {
+                $("article > section").html(html)
+                    .append($('<div style="clear: both;">'));
+            });
+        });
 
-		if (typeof meta.description !== "undefined") {
-			$("meta[name=description]").attr("content", meta.description);
-		}
-	}
+    },
+    get_parameters: function(uri, route) {
+        var self = this;
+        if (typeof route.parameters !== "undefined") {
+            $.each(route.parameters, function(key, regex) {
+                self.parameters[key] = self.hash.match("^" + self.to_regex(uri, route) + "$")[1];
+            });
+        }
+    },
+    load_meta_information: function(meta) {
+
+        if (typeof meta.title !== "undefined") {
+            $("title").html(meta.title);
+            $("meta[name=title]").attr("content", meta.title);
+        }
+
+        if (typeof meta.keywords !== "undefined") {
+            $("meta[name=keywords]").attr("content", meta.keywords);
+        }
+
+        if (typeof meta.description !== "undefined") {
+            $("meta[name=description]").attr("content", meta.description);
+        }
+    }
 }
 
-if (typeof Routes.reload == 'function') { 
-	$(window).bind('hashchange', function() {   
-		Routes.reload();
-	}).load(function() {
-		Routes.reload();
-	});
+if (typeof Routes.reload == 'function') {
+    $(window).bind('hashchange', function() {
+        Routes.reload();
+    }).load(function() {
+        Routes.reload();
+    });
 }
